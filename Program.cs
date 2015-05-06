d99550694063e359237c286d2871f105f4c07993
Naoya KOBAYASHI <bascorpde.crisis@gmail.com>
2015-05-06 20:19:12 +0900

    change main movement

--- Program.cs
+++ Program.cs
@@ -5,56 +5,32 @@ namespace Vatscy.Equation
 {
     public class Program
     {
+        // 2次方程式を解く
         public static void Main(string[] args)
         {
-            // 2点の座標を入力
-            var p1 = ReadPoint(1);
-            var p2 = ReadPoint(2);
-            Console.WriteLine("P1: " + p1);
-            Console.WriteLine("P2: " + p2);
+            var a = ReadConstant("a");
+            var b = ReadConstant("b");
+            var c = ReadConstant("c");
 
-            // 2点を通る直線の方程式を求める
-            var var_a = Polynomial.X;
-            var a = (var_a * p1.X - p1.Y).SolveEquation(var_a * p2.X - p2.Y).Single();
-            var b = (p1.Y - var_a * p1.X)[a];
-            var f = a * Polynomial.X + b;
-            Console.WriteLine("Linear Equation: y = " + f.ToString("x"));
-            Console.WriteLine();
-
-            // 与えられたx座標に対するy座標を求める
-            while (true)
-            {
-                Console.Write("x = ");
-                var x = double.Parse(Console.ReadLine());
-                Console.WriteLine("y = " + f[x]);
-                Console.WriteLine();
-            }
+            var x = Polynomial.X;
+            var p = a * (x ^ 2) + b * x + c;
+            Console.WriteLine(p + " = 0");
+            
+            var solutions = p.SolveEquation(0);
+            Console.WriteLine("x = ["
+                + (solutions.Length == 0 ? "": solutions.Select(s => s.ToString()).Aggregate((now, next) => now + ", " + next))
+                + "]");
         }
 
-        private static Point2D ReadPoint(int index)
+        private static double ReadConstant(string name)
         {
-            Console.Write("X" + index + ": ");
-            var x = Console.ReadLine();
-            Console.Write("Y" + index + ": ");
-            var y = Console.ReadLine();
-            return new Point2D(double.Parse(x), double.Parse(y));
-        }
-
-        private struct Point2D
-        {
-            public double X { get; private set; }
-            public double Y { get; private set; }
-
-            public Point2D(double x, double y) : this()
+            string constant = null;
+            while (string.IsNullOrWhiteSpace(constant))
             {
-                X = x;
-                Y = y;
+                Console.Write(name + ": ");
+                constant = Console.ReadLine();
             }
-
-            public override string ToString()
-            {
-                return "(" + X + ", " + Y + ")";
-            }
+            return double.Parse(constant);
         }
     }
 }
